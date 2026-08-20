3044413c11464a0c03f8d05b669c9860b78525b2
neilpoxon-Personal <neil.poxon@gmail.com>
2026-08-20 15:22:32 +0100

    Change base path in Vite configuration
    changed:
    
    base: '/Shepherd-s-Rod-website/',
    
    To:
    
    base: '/",

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Shepherd-s-Rod-website/',
+    base: '/',
 
     plugins: [react(), tailwindcss()],
 
